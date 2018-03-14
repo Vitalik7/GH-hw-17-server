dd31b4e47b1c9ca1c0328b12c868249d9dcc46e2
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-14 20:26:49 +0200

    update registration

--- server/app.js
+++ server/app.js
@@ -39,8 +39,8 @@ passport.deserializeUser((user, done) => done(null, user))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(bodyParser.json())
-app.use('/api', message)
-app.use('/api', user)
+app.use('/api/v1', message)
+app.use('/api/v1', user)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
