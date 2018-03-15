13ae108606f61fec38dcc1624cc166752f1bf99c
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-15 12:42:45 +0200

    modified MONGODB_URI

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const host = 'localhost'
 
 module.exports = {
   port: process.env.port || process.env.PORT || '3000',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://lavrenko_777:vitalik231@ds211289.mlab.com:11289/react-native-chat`,
+  mongoUrl: process.env.MONGODB_URI || `mongodb://heroku_t52hwv8w:u1t9i3f6m6soi4koqeg1mr1frb@ds239988.mlab.com:39988/heroku_t52hwv8w`,
   facebook: {
     clientID: '169230190394244',
     clientSecret: '46dcdae33520522b24242286e6df0218',
