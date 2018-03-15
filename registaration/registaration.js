242960df2ea52a0eebf113d1f049b6e74cd2409f
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-15 17:24:02 +0200

    remove log

--- registaration/registaration.js
+++ registaration/registaration.js
@@ -35,8 +35,6 @@ router.post('/login',
 router.post('/users', (req, res, next) => {
   let data = req.body.user
 
-  console.log(req.body.user.password);
-
   bcrypt.genSalt(10, function (err, salt) {
     bcrypt.hash(data.password, salt, function (err, hash) {
       data.password = hash
